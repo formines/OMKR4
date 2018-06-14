02e947da05ca57c63d717a769f613cf179bd5153
近江薫 <sh38455@gmail.com>
2018-06-14 21:44:14 +0900

    FirstCommit

---
+++ src/main/kotlin/com/example/OMKR4/Omkr4Application.kt
@@ -0,0 +1,11 @@
+package com.example.OMKR4
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Omkr4Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Omkr4Application::class.java, *args)
+}
---
+++ src/test/kotlin/com/example/OMKR4/Omkr4ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.OMKR4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Omkr4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
